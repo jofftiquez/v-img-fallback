4773bfc809654bcf79e8c68ba6b0aaa4c6ee3727
jofftiquez@gmail.com <jofftiquez@gmail.com>
2017-12-05 14:49:12 +0800

    Added capability to receive a loading placeholder. This directive can now receive both string or object (with loading and error children) value.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export default (Vue) => {
+
   Vue.directive('img-fallback', {
     inserted: (el) => {},
     bind: (el, binding, vnode) => {
@@ -6,14 +7,38 @@ export default (Vue) => {
         name, value, oldValue, expression, arg, modifiers
       } = binding;
 
-      var img = new Image();
+      const defaultLoading = 'http://de.4-traders.com/images/loading_100.gif';
+      const defaultError = 'https://pbs.twimg.com/media/BXhh-sfIAAArh4S.jpg';
+      let loading = defaultLoading;
+      let error = defaultError;
+      let original = el.src;
 
-      img.src = el.src;
+      let img = new Image();
+
+      if(!value) {
+        console.warn(`Vue Img Falback Warning: Directive value is ${ typeof value }. Now using default values.`)
+      }
+
+      if(typeof value === 'string') {
+        loading = value;
+        error = value;
+      }
+
+      if(value instanceof Object) {
+        loading = value.loading || defaultLoading;
+        error = value.error || defaultError;
+      }
       
-      img.onload = () => {};
+      img.src = original;
+
+      el.src = loading;
+      
+      img.onload = () => {
+        el.src = original;
+      };
 
       img.onerror = () => {
-        el.src = value || 'https://pbs.twimg.com/media/BXhh-sfIAAArh4S.jpg';
+        el.src = error;
       };
     }
   });
